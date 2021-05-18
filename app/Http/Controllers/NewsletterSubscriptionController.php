a01221a9f7981d5477e3bb7a6ebf7db8dd878591
José Neder <jlneder@gmail.com>
2021-05-18 20:27:51 -0300

    feat: Add newsletter subscription model and contr.

---
+++ app/Http/Controllers/NewsletterSubscriptionController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\NewsletterSubscription;
+use Illuminate\Http\Request;
+
+class NewsletterSubscriptionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\NewsletterSubscription  $newsletterSubscription
+     * @return \Illuminate\Http\Response
+     */
+    public function show(NewsletterSubscription $newsletterSubscription)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\NewsletterSubscription  $newsletterSubscription
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(NewsletterSubscription $newsletterSubscription)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\NewsletterSubscription  $newsletterSubscription
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, NewsletterSubscription $newsletterSubscription)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\NewsletterSubscription  $newsletterSubscription
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(NewsletterSubscription $newsletterSubscription)
+    {
+        //
+    }
+}
