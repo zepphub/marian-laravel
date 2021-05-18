b565ef553909eb7ec2fc5caf616b93a16e24ef46
José Neder <jlneder@gmail.com>
2021-05-18 13:52:42 -0300

    refactor: Use Category model on the controller

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,27 +2,40 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 use App\Models\Category;
+use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
         $categories = Category::All();
 
-        return view('categories')->with('categories', $categories);
+        return view('admin.categories_index')->with('categories', $categories);
     }
 
-    public function list()
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
     {
-        $categories = Category::All();
-
-        return view('admin.categories_list')->with('categories', $categories);
+        //
     }
 
-    public function store(Category $request)
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
     {
         $categories = Category::All();
         $data = [
@@ -31,16 +44,53 @@ class CategoryController extends Controller
 
         $category = Device::create($data);
 
-        return redirect()->route('admin.categories_list')->with('categories', $categories);
+        return redirect()->route('admin.categories_index')->with('categories', $categories);
     }
 
-    public function delete($id)
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $category)
     {
-        $categories = Category::All();
-        $category = Category::find($id);
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Category $category)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category)
+    {
         $category->delete();
 
-        return view('admin.categories_list')->with('categories', $categories)->with('notice', 'El usuario ha sido eliminado correctamente.');
+        return view('admin.categories_index')->with('notice', 'la categoria ha sido eliminada correctamente.');
     }
-
 }
