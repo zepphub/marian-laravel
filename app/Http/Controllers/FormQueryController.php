f411d4d5298dd15e2fafbd01658bb91ece6c5cbe
José Neder <jlneder@gmail.com>
2021-05-18 20:26:32 -0300

    feat: Add form query model and controller

---
+++ app/Http/Controllers/FormQueryController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\FormQuery;
+use Illuminate\Http\Request;
+
+class FormQueryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\FormQuery  $formQuery
+     * @return \Illuminate\Http\Response
+     */
+    public function show(FormQuery $formQuery)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\FormQuery  $formQuery
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(FormQuery $formQuery)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\FormQuery  $formQuery
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, FormQuery $formQuery)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\FormQuery  $formQuery
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(FormQuery $formQuery)
+    {
+        //
+    }
+}
