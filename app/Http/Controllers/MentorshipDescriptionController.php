b00cc0f9be161f9839742a0eb2aa950d7f3bd240
José Neder <jlneder@gmail.com>
2021-05-18 20:27:20 -0300

    feat: Add mentorship description model and contr.

---
+++ app/Http/Controllers/MentorshipDescriptionController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MentorshipDescription;
+use Illuminate\Http\Request;
+
+class MentorshipDescriptionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\MentorshipDescription  $mentorshipDescription
+     * @return \Illuminate\Http\Response
+     */
+    public function show(MentorshipDescription $mentorshipDescription)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\MentorshipDescription  $mentorshipDescription
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(MentorshipDescription $mentorshipDescription)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\MentorshipDescription  $mentorshipDescription
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, MentorshipDescription $mentorshipDescription)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\MentorshipDescription  $mentorshipDescription
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(MentorshipDescription $mentorshipDescription)
+    {
+        //
+    }
+}
