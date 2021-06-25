cfa96d4b0f48ba1f99eecfc40d8f041383917b5f
José Neder <jlneder@gmail.com>
2021-06-24 21:20:25 -0300

    feat: add html template for order_processed mail

--- resources/views/mail/order_processed.blade.php
+++ resources/views/mail/order_processed.blade.php
@@ -1,10 +1,90 @@
-<p> Hola hola {{ $firstname }} {{ $lastname }}, </p>
-<p> ¡Qué alegría encontrarnos en este espacio!  </p>
-<p> Gracias por contar conmigo para acompañarte en el proceso de profesionalizar tu marca a través de la sesión {{ $service->fullname(false) }}.  </p>
-<p> Con este correo quiero compartirte un formulario previo a la consultoría para poder conocerte un poquito mejor de la mano con tu proyecto y estudiar tu caso, previo a nuestro encuentro.  </p>
-<p> <a href="https://docs.google.com/forms/d/1mokVFdwFO76eMaU1SqnOYKgVswdIGdyvwZ7vATBmWZg/edit?usp=sharing">RESPONDER FORMULARIO</a> </p>
-
-<p> Quedo atenta. Dentro de muy poquito nos veremos en nuestra consultoría programada.  Estoy a tu disposición por cualquier duda o inquietud que surja previo la sesión.  </p>
-<p> ¡Gracias!, una vez más.  </p>
-<p> Saluditos, </p>
-<p> Marian.  </p>
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<meta name="x-apple-disable-message-reformatting">
+	<title></title>
+	<!--[if mso]>
+	<noscript>
+		<xml>
+			<o:OfficeDocumentSettings>
+				<o:PixelsPerInch>96</o:PixelsPerInch>
+			</o:OfficeDocumentSettings>
+		</xml>
+	</noscript>
+	<![endif]-->
+	<style>
+		table, td, div, h1, p {font-family: Arial, Helvetica, sans-serif;}
+	</style>
+</head>
+<body style="margin:0;padding:0;">
+	<table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;background:#ffffff;">
+		<tr>
+			<td align="center" style="padding:0;">
+				<table role="presentation" style="width:602px;border-collapse:collapse;border:1px solid #cccccc;border-spacing:0;text-align:left;">
+					<tr>
+						<td align="center" style="">
+							<img src="{{ asset('img/mail/portada-mail.png') }}" alt="" width="600" style="height:auto;display:block;" />
+						</td>
+					</tr>
+					<tr>
+						<td style="padding:36px 30px 42px 30px;">
+							<table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;">
+								<tr>
+									<td style="padding:0 0 36px 0;color:#153643;">
+										<h1 style="font-size:26px;margin:0 0 20px 0;font-family: Arial, Helvetica, sans-serif; color: #B46063; font-weight: bold;">Hola Hola</h1>
+										<p style="margin:0 0 12px 0;font-size:16px;line-height:24px;font-family: Arial, Helvetica, sans-serif;">{{ $firstname }} {{ $lastname }}, ¿cómo estás? <br>
+                                        ¡Gracias por contactar conmigo!</p>
+                                        <p style="margin:0 0 12px 0;font-size:16px;line-height:24px;font-family: Arial, Helvetica, sans-serif;">Soy Marian, mentora y consultora en Marketing y Estrategias digitales. Trabajo especialmente con mujeres, líderes de marcas autenticas, que tienen intenciones de elevar su esencia en el universo digital a partir de la estrategia.</p>
+                                        <p style="margin:0 0 12px 0;font-size:16px;line-height:24px;font-family: Arial, Helvetica, sans-serif;">Si estás por aquí significa que has leído en profundidad la información sobre el Programa Intensivo de 8 semanas {{ $service->fullname(false) }} en donde podés contar con mi mentoría personalizada durante 2 meses a fin de que creemos o bien, consolidemos tu marca en el universo digital a través de diversas estrategias de marketing digital, comunicación y branding. 
+                                        </p>
+                                        <p style="margin:0 0 12px 0;font-size:16px;line-height:24px;font-family: Arial, Helvetica, sans-serif;">La mentoría personalizada es solo por aplicación.
+                                            A continuación te cuento los pasos que debes seguir para aplicar al programa:</p>
+                                        <p style="margin:0 0 12px 0;font-size:16px;line-height:24px;font-family: Arial, Helvetica, sans-serif;"><span style="font-weight: bold; color:#B46063">1-</span> Hacé click en el botón que encontrarás debajo de los pasos. <br>
+                                            <span style="font-weight: bold; color:#B46063">2-</span> Respondé a las preguntas de aplicación y enviá el formulario. <br>
+                                            <span style="font-weight: bold; color:#B46063">3-</span> Te llegará un email mío durante los 7 días siguientes al envío de tu respuesta. <br>
+                                            <span style="font-weight: bold; color:#B46063">4-</span> En caso de que pueda ayudarte, te enviaré un correo con la disponibilidad de días y horarios a fin de que selecciones la fecha más conveniente para vos (si lo consideramos, en este punto haremos una llamadita de claridad para que podamos evacuar dudas sobre el proceso). <br>
+                                            <span style="font-weight: bold; color:#B46063">5-</span> Una vez confirmado, te enviaré el detalle para que puedas hacer efectivo el pago a fin de reservar tu lugar.
+                                            </p>
+                                        <p style="margin:0 0 12px 0;font-size:16px;line-height:24px;font-family: Arial, Helvetica, sans-serif; color: #B46063;">¡QUIERO COMPLETAR EL FORMULARIO DE APLICACIÓN!
+                                        </p>
+                                        <p style="margin:0 0 12px 0;font-size:16px;line-height:24px;font-family: Arial, Helvetica, sans-serif;">Quedo al pendiente de tu respuesta. Si surge alguna inquietud podés responderme este mismo correo a fin de que lo conversemos y analicemos juntas como avanzar.</p>    
+                                        
+                                        <p style="margin:22px 0 12px 0;font-size:16px;line-height:24px;font-family: Arial, Helvetica, sans-serif;"><a style="text-decoration: none; background-color: #B46063; color: #fff; padding: 10px 20px; border-radius: 50px; font-weight: bold;" href="#">Botón</a></p>  
+										
+									</td>
+                                    <tr>
+                                        <td style="">
+                                            <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;font-size:9px;font-family: Arial, Helvetica, sans-serif;">
+                                                <tr>
+                                                    <td style="padding:0;width:50%;" align="left">
+                                                        <p style="margin:0 0 12px 0;font-size:16px;line-height:24px;font-family: Arial, Helvetica, sans-serif; color:#B46063; font-weight: bold;">Saluditos, <br>
+                                                            Marian.
+                                                            </p>                                                        
+                                                    </td>
+                                                    <td style="padding:0;width:50%;" align="right">
+                                                        <table role="presentation" style="border-collapse:collapse;border:0;border-spacing:0;">
+                                                            <tr>
+                                                                <td style="">
+                                                                    <img src="{{ asset('img/mail/petalo-mail.png') }}" alt="" style="height:auto;display:block;border:0; margin: -44px -35px;" />
+                                                                </td>
+                                                            </tr>
+                                                        </table>
+                                                    </td>
+                                                </tr>
+                                            </table>
+                                        </td>
+                                    </tr>
+                                
+								
+							</table>
+						</td>
+					</tr>
+					
+				</table>
+			</td>
+		</tr>
+	</table>
+</body>
+</html>
