5a055e4a5d27ca3f210b1be16e12cd7765e105d7
zepphub <gustavodenis@gmail.com>
2022-12-02 12:25:18 -0300

    serivicios styles

--- resources/views/front/servicios.blade.php
+++ resources/views/front/servicios.blade.php
@@ -28,35 +28,35 @@
     <div class="row mt-5">
       <div class="col-md-6 mb-5">
           <div class="card border-secondary text-center card-propuestas shadow">
-              <img src="{{ asset('/img/card-talleres-home.svg') }}" class="card-img-top"
+              <img src="{{ asset('/img/ps-badge-dark.svg') }}" class="card-img-top"
                   alt="...">
               <div class="card-body">
-                  <h5 class="card-title ">MENTORÍA <em>personalizada</em></h5>
-                  <h4>Eleva tu Esencia<br>
-                  Programa Selecto para Profesionales y Marcas Digitales</h4>
-                  <p class="card-text">Un proceso en el que te acompaño y ayudo, durante un período de 10 semanas, a crear una estrategia de marca diferencial e integral en entornos digitales a fin de que tu audiencia te perciba como la mejor opción.</p>
+                  <h5 class="card-title epigrafe">MENTORÍA Personalizada</h5>
+                  <h4>Eleva tu Esencia</h4>
+                  <p class="card-text"><b>Programa Selecto para Profesionales y Marcas Digitales.</b><br> Un proceso en el que te acompaño y ayudo, durante un período de 10 semanas, a crear una estrategia de marca diferencial e integral en entornos digitales a fin de que tu audiencia te perciba como la mejor opción.</p>
                   <a href="{{ route('programa-intensivo') }}" class="btn btn-primary">Click aquí para aplicar al proceso</a>
               </div>
           </div>
       </div>
       <div class="col-md-6 mb-5">
           <div class="card border-secondary text-center card-propuestas shadow">
-              <img src="{{ asset('/img/card-talleres-home.svg') }}" class="card-img-top"
+              <img src="{{ asset('/img/badge-mentoria_grupal.png') }}" class="card-img-top"
                   alt="...">
               <div class="card-body">
-                  <h5 class="card-title ">MENTORÍA <em>grupal</em></h5>
+                  <h5 class="card-title epigrafe">MENTORÍA Grupal</h5>
                   <h4>Eleva tu Marca</h4>
-                  <p class="card-text">Un programa grupal de 8 semanas con acompañamiento semanal y sesiones de mentoría en vivo, a través de las cuales, te ayudaré a tomar decisiones estratégicas para transformar tu emprendimiento en un negocio digital sólido y alineado al estilo de vida que deseas.</p>
+                  <p class="card-text"><b>Un programa grupal de 8 semanas con acompañamiento semanal y sesiones de mentoría en vivo.</b><br> Te ayudaré a tomar decisiones estratégicas para transformar tu emprendimiento en un negocio digital sólido y alineado al estilo de vida que deseas.</p>
                   <a href="{{ route('mentoria-grupal') }}" class="btn btn-primary">Click aquí para conocer detalles</a>
               </div>
           </div>
       </div>
       <div class="col-md-6 mb-5">
         <div class="card border-secondary text-center card-propuestas shadow">
-            <img src="{{ asset('/img/card-mentoria-home.svg') }}" class="card-img-top"
+            <img src="{{ asset('/img/card-asesoria.svg') }}" class="card-img-top"
                 alt="...">
             <div class="card-body">
-                <h5 class="card-title ">Cursos <em>on demand</em></h5>
+                <h5 class="card-title epigrafe">Cursos</h5>
+                <h4>On Demand</h4>
                 <p class="card-text">Una propuesta de formación con contenidos audiovisuales grabados (videos, ejemplos con casos reales, ejercicios y guías prácticas) para que puedas capacitarte a tu ritmo y respetar tus procesos creativos sin presiones de días u horarios.</p>
                 <a href="https://servicios.marianpd.com/cursos" target="_blank" class="btn btn-primary">Click aquí para ver los cursos disponible</a>
             </div>
@@ -64,9 +64,10 @@
       </div>
       <div class="col-md-6 mb-5">
         <div class="card border-secondary text-center card-propuestas shadow">
-            <img src="https://marianpd.com/img/card-asesoria-home.svg" class="card-img-top" alt="...">
+            <img src="https://marianpd.com/img/card-talleres.svg" class="card-img-top" alt="...">
             <div class="card-body">
-                <h5 class="card-title ">Conferencias y Talleres <em>a medida</em></h5>
+                <h5 class="card-title epigrafe">Conferencias y Talleres</h5>
+                <h4>A Medida</h4>
                 <p class="card-text">Capacitaciones, workshops y ponencias sobre comunicación experiencial, marketing consciente y estrategia diferencial en entornos digitales.</p>
                 <a href="{{ route('talleres-y-eventos') }}" class="btn btn-primary">Click Aquí</a>
             </div>
