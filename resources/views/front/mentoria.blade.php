200ab308b354ce7a86dfd65251b9313b3b25e59c
zepphub <gustavodenis@gmail.com>
2022-12-06 11:33:21 -0300

    mentorias

--- resources/views/front/mentoria.blade.php
+++ resources/views/front/mentoria.blade.php
@@ -1,72 +1,72 @@
 @extends('layouts.main')
 
-@section('head')@endsection
+@section('head')
+@endsection
 
-@section('title')Mentoría - @endsection
+@section('title')
+    Mentoría -
+@endsection
 
 @section('content')
-<header>
-  <div class="container">
-    <div class="row">
-      <div class="col-md-12 text-center">
-        <h2>Mentoría</h2>
-        <nav class="" aria-label="breadcrumb">
-          <ol class="breadcrumb justify-content-center bg-transparent text-marron-claro">
-            <li class="breadcrumb-item text-marron-claro"><a class="text-marron-claro" href="#">Inicio</a></li>
-            <li class="breadcrumb-item text-secondary active font-weight-bold" aria-current="page">Mentoría</li>
-          </ol>
-        </nav>
-      </div>
-    </div>
-  </div>
-</header>
+    <article>
 
-  <div id="mentoria-portada" class="d-flex align-items-center">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-6">
-          <h4 class="text-white ">Viajamos juntas al corazón de tu marca y aplicamos herramientas adaptadas
-            de forma práctica a tu proyecto. </h4>
-          <img src="{{ asset('img/separador-titulo-blanco-svg.svg') }}" class="separador-ondas" alt="">
-          <a href="#mentoria-propuestas"><button class="btn btn-outline-white d-block mt-4">Ver mis programas </button></a>
-        </div>
-      </div>
-    </div>
-  </div>
+        <header class="container-fluid" id="mentoria-portada">
+            <div class="row py-5">
+                <div class="col-md-6 offset-md-3 text-center">
+                    <h1 class="epigrafe">Mentorías</h1>
+                    <h3 class="text-white mb-5">Viajamos juntas al corazón de tu marca y aplicamos herramientas adaptadas
+                        de forma práctica a tu proyecto. </h3>
+                    <!--<nav class="" aria-label="breadcrumb">
+                          <ol class="breadcrumb justify-content-center bg-transparent">
+                              <li class="breadcrumb-item"><a href="{{ route('index') }}">Inicio</a></li>
+                              <li class="breadcrumb-item active">Mentorías</li>
+                          </ol>
+                      </nav>-->
+                    <a href="#mentoria-propuestas" class="btn btn-outline-white">Ver mis programas</a>
+                </div>
+            </div>
+        </header>
 
-  <div id="mentoria-propuestas">
-    <div class="container py-5">
-      <div class="row">
-        <div class="col-md-12">
-          <h4 class="text-secondary  mb-3">Mis propuestas de mentoría para vos</h4>
-          <p class="text-marron-claro">Creemos una estrategia de marca diferencial, que te ayude a elevar el posicionamiento en entornos digitales, a través de dinámicas específicas centradas en el marketing consciente y en la comunicación experiencial.</p>
-        </div>
-      </div>
-      <div class="row">
-            <div class="col-md-6">
-              <div class="card border-secondary text-center card-propuestas shadow">
-                <img src="{{ asset('img/card-mentoria-home.svg') }}" class="card-img-top w-25 mx-auto my-4 mb-2" alt="...">
-                <div class="card-body">
-                  <h5 class="card-title ">Mentoría Grupal</h5>
-                  <h5 class="card-title ">Eleva tu Marca Online</h5>
-                  <p class="card-text">Un programa grupal de 8 semanas con acompañamiento semanal y sesiones de mentoría en vivo, a través de las cuales, te ayudaré a tomar decisiones estratégicas para transformar tu emprendimiento en un negocio digital sólido y alineado al estilo de vida que deseas.</p>
-                  <a href="{{ route('mentoria-grupal') }}" class="btn btn-primary">¡Quiero saber más!
-                  </a>
+        <section id="mentoria-propuestas" class="container py-5">
+            <div class="row py-5">
+                <div class="col-md-12 text-center">
+                    <h2 class="separador-horizontal">Mis propuestas de mentoría para vos</h2>
+                    <p class="text-marron-claro">Creemos una estrategia de marca diferencial, que te ayude a elevar el
+                        posicionamiento en entornos digitales, a través de dinámicas específicas centradas en el marketing
+                        consciente y en la comunicación experiencial.</p>
                 </div>
-              </div>
             </div>
-            <div class="col-md-6">
-              <div class="card border-secondary text-center card-propuestas shadow">
-                <img src="{{ asset('img/card-asesoria-home.svg') }}" class="card-img-top w-25 mx-auto my-4 mb-2" alt="...">
-                <div class="card-body">
-                  <h5 class="card-title ">Mentoría Personalizada</h5>
-                  <h5 class="card-title ">Programa Selecto para Profesionales y Marcas Digitales</h5>
-                  <p class="card-text">Un proceso en el que te acompaño y ayudo, durante un período de 10 semanas, a crear una estrategia de marca diferencial e integral en entornos digitales a fin de que tu audiencia te perciba como la mejor opción.</p>
-                  <a href="{{ route('programa-intensivo') }}" class="btn btn-primary">¡Quiero saber más!</a>
+            <div class="row">
+                <div class="col-md-6 mb-5">
+                    <div class="card border-secondary text-center card-propuestas shadow">
+                        <img src="{{ asset('/img/ps-badge-dark.svg') }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title epigrafe">MENTORÍA Personalizada</h5>
+                            <h4>Eleva tu Esencia</h4>
+                            <p class="card-text"><b>Programa Selecto para Profesionales y Marcas Digitales.</b><br> Un
+                                proceso en el que te acompaño y ayudo, durante un período de 10 semanas, a crear una
+                                estrategia de marca diferencial e integral en entornos digitales a fin de que tu audiencia
+                                te perciba como la mejor opción.</p>
+                            <a href="{{ route('programa-intensivo') }}" class="btn btn-primary">Click aquí para aplicar al
+                                proceso</a>
+                        </div>
+                    </div>
                 </div>
-              </div>
+                <div class="col-md-6 mb-5">
+                    <div class="card border-secondary text-center card-propuestas shadow">
+                        <img src="{{ asset('/img/badge-mentoria_grupal.png') }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title epigrafe">MENTORÍA Grupal</h5>
+                            <h4>Eleva tu Marca</h4>
+                            <p class="card-text"><b>Un programa grupal de 8 semanas con acompañamiento semanal y sesiones de
+                                    mentoría en vivo.</b><br> Te ayudaré a tomar decisiones estratégicas para transformar tu
+                                emprendimiento en un negocio digital sólido y alineado al estilo de vida que deseas.</p>
+                            <a href="{{ route('mentoria-grupal') }}" class="btn btn-primary">Click aquí para conocer
+                                detalles</a>
+                        </div>
+                    </div>
+                </div>
             </div>
-      </div>
-    </div>
-  </div>
+        </section>
+    </article>
 @endsection
