a61855070b809bfb43ceed6d20479c9a8f792225
zepphub <gustavodenis@gmail.com>
2022-12-08 10:07:49 -0300

    home page

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,31 +2,35 @@
         <div class="row">
             <div class="col-xl-4 col-lg-3 col-sm-6">
                 <div class="widget">
-                <img src="{{ asset('img/logo-footer.svg') }}" alt="">
-                <p class="mt-4 mb-md-5">Me especializo en la creación de proyectos y estrategias digitales y lo hago a través de
-                    experiencias, asesorías y propuestas
-                    especialmente diseñadas para marcas con
-                    historia, con propósito y corazón.</p>
-                <div class="social-footer my-4 my-md-0">
-                    <a href="https://twitter.com/Marianaapd?s=08" target="_blank"><button class="btn rounded-circle pl-0 pr-md-0"><svg xmlns="http://www.w3.org/2000/svg"
-                                width="27" height="20" fill="currentColor" class="text-secondary bi bi-twitter"
-                                viewBox="0 0 16 16">
-                                <path
-                                    d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" />
-                            </svg></button></a>
-                    <a href="https://www.instagram.com/mariann.pd/?hl=es-la" target="_blank"><button class="btn rounded-circle pr-md-0"><svg xmlns="http://www.w3.org/2000/svg" width="18"
-                                height="18" fill="currentColor" class="text-secondary bi bi-instagram"
-                                viewBox="0 0 16 16">
-                                <path
-                                    d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z" />
-                            </svg></button></a>
-                    <a href="https://www.linkedin.com/in/mariana-pacheco-d-aquila-ba537375/" target="_blank"><button class="btn rounded-circle pr-md-0"><svg xmlns="http://www.w3.org/2000/svg" width="19.367"
-                                height="19.367" viewBox="0 0 19.367 19.367">
-                                <path id="Icon_awesome-linkedin-in" data-name="Icon awesome-linkedin-in"
-                                    d="M4.335,19.367H.32V6.437H4.335ZM2.325,4.673A2.336,2.336,0,1,1,4.651,2.326,2.345,2.345,0,0,1,2.325,4.673ZM19.363,19.367H15.356V13.073c0-1.5-.03-3.424-2.088-3.424-2.088,0-2.407,1.63-2.407,3.316v6.4H6.85V6.437H10.7V8.2h.056a4.219,4.219,0,0,1,3.8-2.088c4.064,0,4.811,2.676,4.811,6.152v7.1Z"
-                                    transform="translate(0 -0.001)" fill="#b46063" />
-                            </svg>
-                        </button></a>
+                    <img src="{{ asset('img/logo-footer.svg') }}" alt="">
+                    <p class="mt-4 mb-md-5">Me especializo en la creación de proyectos y estrategias digitales y lo hago
+                        a través de
+                        experiencias, asesorías y propuestas
+                        especialmente diseñadas para marcas con
+                        historia, con propósito y corazón.</p>
+                    <div class="social-footer my-4 my-md-0">
+                        <a href="https://twitter.com/Marianaapd?s=08" target="_blank"><button
+                                class="btn rounded-circle pl-0 pr-md-0"><svg xmlns="http://www.w3.org/2000/svg"
+                                    width="27" height="20" fill="currentColor"
+                                    class="text-secondary bi bi-twitter" viewBox="0 0 16 16">
+                                    <path
+                                        d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" />
+                                </svg></button></a>
+                        <a href="https://www.instagram.com/mariann.pd/?hl=es-la" target="_blank"><button
+                                class="btn rounded-circle pr-md-0"><svg xmlns="http://www.w3.org/2000/svg"
+                                    width="18" height="18" fill="currentColor"
+                                    class="text-secondary bi bi-instagram" viewBox="0 0 16 16">
+                                    <path
+                                        d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z" />
+                                </svg></button></a>
+                        <a href="https://www.linkedin.com/in/mariana-pacheco-d-aquila-ba537375/" target="_blank"><button
+                                class="btn rounded-circle pr-md-0"><svg xmlns="http://www.w3.org/2000/svg"
+                                    width="19.367" height="19.367" viewBox="0 0 19.367 19.367">
+                                    <path id="Icon_awesome-linkedin-in" data-name="Icon awesome-linkedin-in"
+                                        d="M4.335,19.367H.32V6.437H4.335ZM2.325,4.673A2.336,2.336,0,1,1,4.651,2.326,2.345,2.345,0,0,1,2.325,4.673ZM19.363,19.367H15.356V13.073c0-1.5-.03-3.424-2.088-3.424-2.088,0-2.407,1.63-2.407,3.316v6.4H6.85V6.437H10.7V8.2h.056a4.219,4.219,0,0,1,3.8-2.088c4.064,0,4.811,2.676,4.811,6.152v7.1Z"
+                                        transform="translate(0 -0.001)" fill="#b46063" />
+                                </svg>
+                            </button></a>
                     </div>
                 </div>
             </div>
@@ -52,7 +56,7 @@
                     </ul>
                 </div>
             </div>
-            <div class="col-xl-2 col-lg-3 col-sm-6">    
+            <div class="col-xl-2 col-lg-3 col-sm-6">
                 <div class="widget">
                     <h5>Trabajá conmigo</h5>
                     <ul class="nav flex-column">
@@ -67,7 +71,7 @@
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('talleres-y-eventos') }}">Conferencias</a>
-                        </li>                    
+                        </li>
                     </ul>
                 </div>
             </div>
@@ -76,14 +80,21 @@
                     <h5>Mis novedades</h5>
                     <div class="py-1 px-1">
                         @php
-                        $footer_posts = \App\Models\Post::latest()->take(3)->get();
+                            $footer_posts = \App\Models\Post::latest()
+                                ->take(3)
+                                ->get();
                         @endphp
                         @foreach ($footer_posts as $post)
-                        <div>
-                            <a href="{{ route('blog.post', [ 'category' => $post->category->slug, 'post' => $post->slug ]) }}"><h6 class="text-marron">{{ $post->title }}</h6></a>
-                            <p class="date-input"><!--<span>{{ $post->author }}</span>.--><span>{{ $post->created_at->format('d M Y') }}</span></p>
-                            <hr>
-                        </div>
+                            <div>
+                                <a
+                                    href="{{ route('blog.post', ['category' => $post->category->slug, 'post' => $post->slug]) }}">
+                                    <h6 class="text-marron">{{ $post->title }}</h6>
+                                </a>
+                                <p class="date-input">
+                                    <!--<span>{{ $post->author }}</span>.--><span>{{ $post->created_at->format('d M Y') }}</span>
+                                </p>
+                                <hr>
+                            </div>
                         @endforeach
                     </div>
                 </div>
